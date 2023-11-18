7367de066cc5b0d73ddc889b605477f7575dbab9
mikethesupertramp <49910971+mikethesupertramp@users.noreply.github.com>
2023-11-18 22:24:26 +0400

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "====Bendcontrol initialization script===="
+echo "====Updating pacman===="
+sudo pacman -Syu --noconfirm
+echo "====Installing Java===="
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 17.0.7-tem
+echo "====Installing Git===="
+sudo pacman -S git --noconfirm
+git config --global credential.helper store
+echo "====Downloading project files===="
+echo "Enter your github username and (as a password) personal access token"
+git clone https://github.com/langmuirsystems/bendcontrol.git
+cd bendcontrol
+git fetch
+git pull origin debug/pi
+git checkout origin/debug/pi
+echo "====Building project for faster startup"
+bash gradlew build
+echo "====adding user to UUCP group to allow usb device connections===="
+sudo gpasswd -a $USER uucp
+echo "====Removing Desktop Environment"
+sudo pacman -Rcs xfce4 --noconfirm
+sudo pacman -Rcs xfce4-goodies --noconfirm
+sudo pacman -Rcs lightdm --noconfirm
+echo "====Installing XINIT====="
+sudo pacman -S xorg-xinit --noconfirm
+echo "====Writing .xinitrc====="
+echo "cd bendcontrol; bash gradlew run"
+echo "====Restarting===="
+sudo reboot
